866159454839925371d9eecf42098ae9d7daea30
jindonglee <dropnowon@gmail.com>
2024-01-11 15:59:29 +0900

    주석

--- category.js
+++ category.js
@@ -4,14 +4,14 @@ const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
 
 let currentIndex = 0;
-
+// 이전으로 가는 버튼 설정
 prev.addEventListener("click", () => {
   if (currentIndex > 0) {
     currentIndex--;
     updateSlider();
   }
 });
-
+// 다음으로 가는 버튼 설정
 next.addEventListener("click", () => {
   if (currentIndex < container.children.length - 2) {
     currentIndex++;
@@ -19,6 +19,7 @@ next.addEventListener("click", () => {
   }
 });
 
+// 슬라이더가 실행되도록 한다.
 function updateSlider() {
   const slideWidth = container.children[0].offsetWidth;
   const maxTranslateValue = slideWidth * (container.children.length - 2); // 특정 값
@@ -28,7 +29,7 @@ function updateSlider() {
   if (Math.abs(newTranslateValue) < maxTranslateValue) {
     container.style.transform = `translateX(${newTranslateValue}px)`;
   } else {
-    // 특정 조건을 만족하면 더 이상 슬라이딩을 진행하지 않도록 할 수 있습니다.
+    // 슬라이딩 진행하지 않게 하는 코드
     console.log("Sliding stopped!");
   }
 }
