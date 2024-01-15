78978ebe58e826fbfed455f134cd921fbaae5632
wltjsl <ljss7291@gmail.com>
2024-01-15 22:35:21 +0900

    유저 별점 숫자 더 정확하게 수정

--- modal.js
+++ modal.js
@@ -121,18 +121,25 @@ function displayModal(movie) {
 
     // 유저 평점이 존재하면 표시
     if (!isNaN(userAverage)) {
-      userAverage = Math.round(userAverage * 2);
+      // 영화 평점과 맞추기 위해 2배
+      userAverage *= 2;
+
+      // 별로 표시하기 위한 반올림
+      let userAverageRound = Math.round(userAverage);
+
+      // 숫자로 표시하기 위한 계산
+      userAverage = Math.round(userAverage * 10) / 10;
 
       let userStar = "";
 
-      if (userAverage % 2 === 0) {
+      if (userAverageRound % 2 === 0) {
         for (let i = 0; i < userAverage / 2; i++) {
           userStar += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
           <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
         </svg>`;
         }
       } else {
-        for (let i = 0; i < (userAverage - 1) / 2; i++) {
+        for (let i = 0; i < (userAverageRound - 1) / 2; i++) {
           userStar += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
           <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
         </svg>`;
